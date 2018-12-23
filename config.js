1e0a175a65138429b9d686b42db966ec12af3125
Chris Yang <cyang287@yahoo.com>
2018-12-23 16:11:26 -0500

    first commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://webuser:webuser123@ds229722.mlab.com:29722/backend-template';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://webuser:webuser123@ds041586.mlab.com:41586/youtube-sync';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/backend-test';
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
